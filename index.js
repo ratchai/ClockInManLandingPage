d0515f7243c93153e6db0b99837469ae8947baa3
ratchai <ratchai@gmail.com>
2021-04-24 00:23:08 +0700

    2 files modified

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import "./css/bootstrap.min.css";
 import "./css/owl.carousel.min.css";
 import "./fonts/thsarabunnew.css";
 import "./css/style.css";
-import "./css/themify-icons.css";
+import "./css/themify-icons.css"; 
