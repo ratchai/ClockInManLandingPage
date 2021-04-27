472d184aeff9598ede1791095b68ee1e156bbe4c
ratchai <ratchai@gmail.com>
2021-04-27 23:42:32 +0700

    Start firebase

--- portal/index.js
+++ portal/index.js
@@ -1,10 +1,10 @@
 var tabledata = [
-    {Date:"14-05-64", Time:"09.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
-    {Date:"14-05-64",Time:"10.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
-    {Date:"14-05-64",Time:"11.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
-    {Date:"14-05-64",Time:"12.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
-    {Date:"14-05-64",Time:"13.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
-    {Date:"14-05-64",Time:"14.00" ,Action:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64", Time:"09.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64",Time:"10.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64",Time:"11.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64",Time:"12.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64",Time:"13.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
+    {Date:"14-05-64",Time:"14.00" ,Type:"in",Location:"ต.สวนดอก อ.เมือง จ.ลำปาง"},
     
     
 ];
@@ -12,7 +12,19 @@ var tabledata = [
 //initialize table
 var table = new Tabulator("#example-table", {
     data:tabledata, //assign data to table
-    autoColumns:true, //create columns from data field names
     layout:"fitDataStretch",
-    fitColumns :true
+    columns:[
+        {title:"Date", field:"Date", hozAlign:"center"},     
+        {title:"Time", field:"Time", hozAlign:"center"},        
+        {title:"Type", field:"Type", hozAlign:"center",formatter:function(cell, formatterParams){
+            var value = cell.getValue();
+             if(value.indexOf("i") >= 0){
+                 return "<span style='color:limegreen; font-weight:bold;'>" + value + "</span>";
+             }else{
+                return "<span style='color:orange; font-weight:bold;'>" + value + "</span>";
+             }
+         }},
+         {title:"Location",field:"Location"}
+    ]
+    
 });
